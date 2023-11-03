9b7f3fa4138c557ba58a63063430f3cb2e37b6bf
Clarice Bascou <clarice.bascou@gmail.com>
2023-11-03 11:14:20 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/year', (req, res) => {
+  const now = new Date();
+  const year = now.getFullYear();
+  res.json({year});
+});
+
+module.exports = router;
